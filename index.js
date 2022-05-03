d31f8f83bd6250014ddc2fbbafff993e083a9458
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-05-03 00:39:09 -0700

    code finally works
    will finish routes tonight

--- index.js
+++ index.js
@@ -5,144 +5,103 @@ const express = require("express"),
   morgan = require("morgan"),
   fs = require("fs"),
   path = require("path"),
-  uuid = require('uuid'),
-  bodyParser = ('body-parser')
-  const app = express();
-
-
+  uuid = require("uuid"),
+  bodyParser = "body-parser";
+const app = express();
 
 let movies = [
- 
   {
-    title: "Paranormal Activity",
-    producer: "Oren Peli",
-    production: "Blumhouse",
-    release: "October 14, 2007",
+      title: "Paranormal Activity",
+      description:
+      "A young couple who are haunted by a supernatural presence in their home. They then set up a camera to document what is haunting them.",
+  
+      genre: "horror",
+      director: {
+      name: "Oren Peli",
+      birth: "January 21, 1970",
+      image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmubi.com%2Fcast%2Foren-peli&psig=AOvVaw3OQdh4EVmziRC9Ng6v8DyX&ust=1651610864426000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDWxYnYwfcCFQAAAAAdAAAAABAD",
+      featured: true
+    }
   },
+   
 
   {
     title: "Grave Encounters",
-    producer: "Shawn Angelski, Michael Karlin",
-    production:
-      "Darclight, Twin Engine Films, Digital Interference Productions",
-    release: "April 22, 2011",
+    description: " The footage follows the crew of a paranormal reality television program who lock themselves in a haunted psychiatric hospital in search of evidence of paranormal activity as they shoot what ends up becoming their final episode.",
+    genre: "horror",
+    director: {
+      name: "The Vicious Brothers (Colin Minihan, Stuart Ortiz)",
+      birth: "Colin Minihan (1986), Stuart Ortiz (March 20, 1984)",
+      image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmubi.com%2Fcast%2Fthe-vicious-brothers&psig=AOvVaw3tY4Yt32-0f4J-KzYdvlUU&ust=1651611250372000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKDHxcHZwfcCFQAAAAAdAAAAABAD",
+      featured: true
+    }
   },
 
   {
     title: "Possession of Michael King",
-    producer:
-      "Paul Brooks, Jaime Burke, Guy Danella, Jef Levine, Scott Niemeyer, Tedi Sarafian",
-    production: "Gold Circle Films, Quickfire Films",
-    release: "August 14, 2014",
+    description: "After poor advice from a psychic leads to the death of his wife, a man vows to disprove the existence of the paranormal by allowing himself to be possessed by demons.",
+    genre: "horror",
+    director: {
+      name: "David Jung",
+      birth: "December 23, 1971",
+      image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmubi.com%2Fcast%2Fdavid-jung&psig=AOvVaw0qdaI525dDe8Fxe3sGWFbT&ust=1651611762116000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNiE9bXbwfcCFQAAAAAdAAAAABAJ",
+      featured: true
+    }  
+  },
+
+ {
+    title: "Aterrados (Terrified)",
+    description: "Terrified introduces us to a neighborhood that is plagued by an infection of sorts. The symptoms of this infection include the dead brought to life, strange creatures that resemble corpses, and invisible/visible spooks that seem to live in the dark corners of houses and apartments.",
+    genre: "horror",
+    director: {
+      name: "Demián Rugna",
+      birth: "September 13, 1979",
+      image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhorror.fandom.com%2Fwiki%2FDemi%25C3%25A1n_Rugna&psig=AOvVaw3u4arKd4YQmpKfLg1uq9Wh&ust=1651611996742000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKCvhqbcwfcCFQAAAAAdAAAAABAD",
+      featured: true
+    }
   },
 
   {
     title: "The Conjuring",
-    producer: "Tony DeRosa-Grund, Peter Safran, Rob Cowan",
-    production: "New Line Cinema, The Safran Company, Evergreen Media Group",
-    release: "July 15, 2013",
-  },
+    description: "Paranormal investigators Ed and Lorraine Warren work to help a family terrorized by a dark presence in their farmhouse", 
+    genre: "horror",
+    director: {
+      name: "James Wan",
+      birth: "February 26, 1977",
+      image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdcextendeduniverse.fandom.com%2Fwiki%2FJames_Wan&psig=AOvVaw1aIcCLIdzFBf813Jv5vlzV&ust=1651612157193000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCIDf2PHcwfcCFQAAAAAdAAAAABAP",
+      featured: true
+    }
+  }];
+  
 
-  {
-    title: "The Conjuring 2",
-    producer: "Peter Safran, Rob Cowan, James Wan",
-    production:
-      "New Line Cinema, RatPac-Dune Entertainment, The Safran Group, Atomic Monster Productions",
-    release: "June 7, 2016",
-  },
-
-  {
-    title: "Insidious",
-    producer: "Jason Blum, Steven Schneider, Oren Peli",
-    production: "Haunted Movies, Stage 6 Films, Alliance Films, IM Global",
-    release: "September 14, 2010",
-  },
-
-  {
-    title: "Insidious Chapter: 2",
-    producer: "Jason Blum, Oren Peli",
-    production:
-      "Blumhouse Productions, Entertainment One, FilmDistrict, Stage 6 Films, Entertainment One",
-    relase: "September 13, 2013",
-  },
-
-  {
-    title: "Quarantine",
-    producer: "Sergio Aguero, Doug Davison, Roy Lee",
-    production: "Sony Pictures Releasing",
-    release: "October 10, 2008",
-  },
-
-  {
-    title: "Aterrados (Terrified)",
-    producer: "Fernando Diaz",
-    production: "Aura Films",
-    release: "May 3, 2018",
-  },
-
-  {
-    title: "Hereditary",
-    producer: "Kevin Frakes, Lars Knudsen, Buddy Patrick",
-    production: "A24, PalmStar Media, Finch Entertainment, Windy Hill Pictures",
-    release: "January 18, 2018",
-  },
-];
-
-
-
-
-
+ 
 
 //START NEW GET REQUESTS
 
 // Get requests
 
-app.get('/', (req,res) => {
-  res.send('Welcome to the Horror Movie Movie Vault');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Horror Movie Movie Vault");
 });
 
+// Returns list of all data 
 
-
-app.get('/documentation', (req,res) => {
-  
-})
-
-// Gets the list of data about ALL students:
-
-app.get('/movies', (req, res) => {
+app.get("/movies", (req, res) => {
   res.json(movies);
-  res.send('Successfully Get request returning data on all movies');
+  // res.send("Successfully Get request returning data on all movies");
 });
 
-// Get list of movies by title:
+// Returns list of movie titles
 
-app.get('/title/', (req,res) => {
-  res.json(movies.find((movies) =>
+app.get("/movies/:title", (req,res) => {
+  res.json(movies.find( (movies) =>
   {return movies.title === req.params.title} ));
-  res.send('Successfully Get request returning data on movie titles');
-});
-
-
-// Get list of movies by production company: 
-
-app.get('/movies/:production', (req, res) => {
-  res.json(movies.find( (movies) =>
-  { return movies.production === req.params.production} ));
-  res.send('Successfully Get request returning data on production company');
-});
-
-// Get list of movies by release date:
-
-app.get('/movies/:release', (req, res) => {
-  res.json(movies.find( (movies) =>
-  { return movies.release === req.params.release} ));
-  res.send('Successfully Get request returning data on all movie release');
+  res.send
 });
 
 
 // Documentation
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 //Morgan logging
 app.use(morgan("common"));
