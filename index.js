2f5b9e48bbe251f3091cfbe441e5d6881a567264
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-05-03 12:16:13 -0700

    final
    updated all routes, and description. added all screenshots with successful routing in postman

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const express = require("express"),
   fs = require("fs"),
   path = require("path"),
   uuid = require("uuid"),
-  bodyParser = "body-parser";
+  bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.json());
 
 let movies = [
   {
     title: "Paranormal Activity",
     description:
       "A young couple who are haunted by a supernatural presence in their home. They then set up a camera to document what is haunting them.",
-    genre: "horror",
+    genre: {
+      name: "horror",
+      description:
+        "Unsettling films designed to frighten and panic, cause dread and alarm, and to invoke our hidden worst fears, often in a terrifying, shocking finale, while captivating and entertaining us at the same time in a cathartic experience.",
+    },
     director: {
       name: "Oren Peli",
       birth: "January 21, 1970",
@@ -28,7 +34,11 @@ let movies = [
     title: "Grave Encounters",
     description:
       " The footage follows the crew of a paranormal reality television program who lock themselves in a haunted psychiatric hospital in search of evidence of paranormal activity as they shoot what ends up becoming their final episode.",
-    genre: "horror",
+    genre: {
+      name: "horror",
+      description:
+        "Unsettling films designed to frighten and panic, cause dread and alarm, and to invoke our hidden worst fears, often in a terrifying, shocking finale, while captivating and entertaining us at the same time in a cathartic experience.",
+    },
     director: {
       name: "The Vicious Brothers (Colin Minihan, Stuart Ortiz)",
       birth: "Colin Minihan (1986), Stuart Ortiz (March 20, 1984)",
@@ -42,7 +52,11 @@ let movies = [
     title: "Possession of Michael King",
     description:
       "After poor advice from a psychic leads to the death of his wife, a man vows to disprove the existence of the paranormal by allowing himself to be possessed by demons.",
-    genre: "horror",
+    genre: {
+      name: "horror",
+      description:
+        "Unsettling films designed to frighten and panic, cause dread and alarm, and to invoke our hidden worst fears, often in a terrifying, shocking finale, while captivating and entertaining us at the same time in a cathartic experience.",
+    },
     director: {
       name: "David Jung",
       birth: "December 23, 1971",
@@ -53,10 +67,14 @@ let movies = [
   },
 
   {
-    title: "Aterrados (Terrified)",
+    title: "Aterrados",
     description:
       "Terrified introduces us to a neighborhood that is plagued by an infection of sorts. The symptoms of this infection include the dead brought to life, strange creatures that resemble corpses, and invisible/visible spooks that seem to live in the dark corners of houses and apartments.",
-    genre: "horror",
+    genre: {
+      name: "horror",
+      description:
+        "Unsettling films designed to frighten and panic, cause dread and alarm, and to invoke our hidden worst fears, often in a terrifying, shocking finale, while captivating and entertaining us at the same time in a cathartic experience.",
+    },
     director: {
       name: "Demián Rugna",
       birth: "September 13, 1979",
@@ -73,7 +91,7 @@ let movies = [
     genre: {
       name: "horror",
       description:
-        "From the Dark, mysterious, and paranormal, these horror flicks will get you scared",
+        "Unsettling films designed to frighten and panic, cause dread and alarm, and to invoke our hidden worst fears, often in a terrifying, shocking finale, while captivating and entertaining us at the same time in a cathartic experience.",
     },
     director: {
       name: "James Wan",
@@ -82,6 +100,26 @@ let movies = [
         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdcextendeduniverse.fandom.com%2Fwiki%2FJames_Wan&psig=AOvVaw1aIcCLIdzFBf813Jv5vlzV&ust=1651612157193000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCIDf2PHcwfcCFQAAAAAdAAAAABAP",
       featured: true,
     },
+  },
+];
+
+let users = [
+  {
+    id: 1,
+    name: "Cami",
+    favoriteMovies: [],
+  },
+
+  {
+    id: 2,
+    name: "Mario",
+    favoriteMovies: ["Grave Encounters"],
+  },
+
+  {
+    id: 3,
+    name: "Maverick",
+    favoriteMovies: [],
   },
 ];
 
@@ -102,32 +140,132 @@ app.get("/movies", (req, res) => {
 // Returns list of movie titles
 
 app.get("/movies/:title", (req, res) => {
-  res.json(
-    movies.find((movies) => {
-      return movies.title === req.params.title;
-    })
-  );
+  const { title } = req.params;
+  const movie = movies.find((movie) => movie.title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else res.status(400).send("no such movie found");
 });
 
 // Returns movie genre
 
-app.get("/movies/:genre", (req, res) => {
-  res.json(
-    movies.find((movies) => {
-      return movies.genre === req.params.genre;
-    })
-  );
+app.get("/movies/genre/:genreName", (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find((movie) => movie.genre.name === genreName).genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else res.status(400).send("no such genre found");
 });
 
 // Returns movie director
 
-app.get("/director/:name", (req,res) => {
- res.json(director.find((director) => {
-   return director.name === req.params.director
- }));
+app.get("/movies/director/:directorName", (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find(
+    (movie) => movie.director.name === directorName
+  ).director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else res.status(400).send("no such director found");
 });
 
-// Documentation
+// ----- End Get Requests -----
+//------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------
+
+// ----- Start Users -----
+
+// Add users
+
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("users need names");
+  }
+});
+
+// Update users
+
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("no such user found");
+  }
+});
+
+// Users add movie to favorites
+
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res
+      .status(200)
+      .send(`${movieTitle} has been added to the user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user found");
+  }
+});
+
+// Users delete movie from favorites
+
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(
+      (title) => title !== movieTitle
+    );
+    res
+      .status(200)
+      .send(`${movieTitle} has been removed from the user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user found");
+  }
+});
+
+// Users can de-register
+
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    users = users.filter((user) => user.id != id);
+    res.status(200).send(` user ${id} as been deleted`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+// ----- End Users -----
+//------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------
+
+// Misc
+
+//Documentation
 app.use(express.static("public"));
 
 //Morgan logging
