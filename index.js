6141cedad3c55648045d86108334f421fb88fe13
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-05-12 02:44:26 -0700

    Updated
    Updated to Heroku portion asking to save all, and commit.

--- index.js
+++ index.js
@@ -16,6 +16,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// CORS ---------------------
+//---------------------------
+
+const cors = require('cors');
+app.use(cors());
+
+let allowedOrigins = 'http://localhost:8080';
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+// Express validator ---------
+//----------------------------
+
+const { check, validationResult} = require('express-validator');
+
 // Authenticate and Authorize
 //---------------------------
 
@@ -114,7 +138,19 @@ app.get('/director/:Name', passport.authenticate('jwt', {session: false}), (req,
 
 // Allow new users to register
 
-app.post('/users', (req, res) => {
+app.post('/users',
+[
+check('Username', 'Username is required').isLength({min: 5}),
+check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+check('Password', 'Password is required').not().isEmpty(),
+check('Email', 'Email does not appear to be valid').isEmail()
+],
+(req, res) => {
+  let errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(422).json({errors: errors.array() });
+    }
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -123,7 +159,7 @@ app.post('/users', (req, res) => {
         Users
           .create({
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
@@ -232,8 +268,9 @@ app.use(morgan("common"));
 
 //Listen for requests ---
 //-----------------------
-app.listen(8080, () => {
-  console.log("Your app is listening on port 8080.");
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log("Listening on Port " + port);
 });
 
 
