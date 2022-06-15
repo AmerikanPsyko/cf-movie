2ab58088f0b2cf5abf000fc97232d9a9503048ba
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-06-15 12:06:05 -0700

    added heroku app to cors
    added heroku app to cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 app.use(cors());
 
-let allowedOrigins = 'http://localhost:8080, http://localhost:1234';
+let allowedOrigins = 'http://localhost:8080, http://localhost:1234, https://cfmyflix.herokuapp.com/';
 
 app.use(cors({
   origin: (origin, callback) => {
