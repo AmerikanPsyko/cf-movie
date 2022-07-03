6a53a920eeac317dbfaa164a6633d012dc89a520
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-07-03 12:33:40 -0700

    removed cors
    removed cors

--- index.js
+++ index.js
@@ -24,18 +24,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 app.use(cors());
 
-let allowedOrigins = 'localhost:8080, localhost:1234, https://cfmyflix.herokuapp.com/, http://localhost:1234, https://cfmyflix.netlify.app/';
+// let allowedOrigins = 'localhost:8080, localhost:1234, https://cfmyflix.herokuapp.com/, http://localhost:1234, https://cfmyflix.netlify.app/';
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 // Express validator ---------
 //----------------------------
